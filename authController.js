b4bd1dc47e31b6949ef14349d9c240908be848c6
Jopkins <sergebrew@gmail.com>
2022-03-13 02:29:12 +0300

    login validation w/ jwt token

--- authController.js
+++ authController.js
@@ -2,7 +2,17 @@
 const User = require('./models/User');
 const Role = require('./models/Role');
 const bcrypt = require('bcryptjs'); //для хеширования пароля
+const jwt = require('jsonwebtoken'); // JWT token
 const { validationResult } = require('express-validator'); // функция для возвращения ошибок в следствии валидации
+const { secret } = require('./config');
+
+const generateAccessToken = (id, roles) => {
+  const payload = {
+    id,
+    roles
+  }
+  return jwt.sign(payload, secret, {expiresIn: "24h"});
+}
 
 class authController {
   async registration(req, res) {
@@ -29,6 +39,17 @@ class authController {
 
   async login(req, res) {
     try {
+      const {username, password} = req.body; // Достаем юзернейм и пароль из тела запроса
+      const user = await User.findOne({username}); // Ищем юзера в базе данных
+      if (!user) { // Проверяем, если юзер не найден - возвращаем сообщение
+        return res.status(400).json({message: `User ${username} not found`});
+      }
+      const validPassword = bcrypt.compareSync(password, user.password); // Сравниваем пароли введенный и из БД
+      if (!validPassword) { // Если пароль не верный - возвращаем сообщение
+        return res.status(400).json({message: "Incorrect password"});
+      }
+      const token = generateAccessToken(user._id, user.roles);
+      return res.json({token});
     } catch (e) {
       console.log(e);
       res.status(400).json({message: 'Login error'})
