7287b58a41ff5d23aef439991ed0730cde4e9e5f
Jopkins <sergebrew@gmail.com>
2022-03-12 19:23:14 +0300

    connected to mongoDB and s11 json parser

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 // Creating project installing packages 'express mongoose nodemon'
+// mongoose - библиотека для работы с базой данных mongo. ODM аналог ORM для документо-ориентированный бд
 
 const express = require('express');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || '5000';
 
 
-const app = express()
+const app = express();
+
+app.use(express.json());
 
 
-
-const start = () => {
+const start = async () => {
   try {
+    await mongoose.connect(`mongodb+srv://idaveee:idaveee5455@cluster0.2ulgv.mongodb.net/auth-learn?retryWrites=true&w=majority`);
     app.listen(PORT, () => console.log(`server started on port ${PORT}`));
   } catch (e) {
     console.log(e)
