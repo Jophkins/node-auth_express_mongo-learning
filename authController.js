0ecb5ae4f68edf3d4a57031bb5bff068a93c5bde
Jopkins <sergebrew@gmail.com>
2022-03-12 21:45:01 +0300

    user registration logic w/o validation

--- authController.js
+++ authController.js
@@ -1,13 +1,24 @@
 
 const User = require('./models/User');
 const Role = require('./models/Role');
+const bcrypt = require('bcryptjs'); //для хеширования пароля
 
 class authController {
   async registration(req, res) {
     try {
-
+      const {username, password} = req.body;
+      const candidate = await User.findOne({username}); // ищем юзера
+      if (candidate) {
+        return res.status(400).json({message: 'Username already exist'}); // если нашли - ошибка
+      }
+      const hashPassowrd = bcrypt.hashSync(password, 7); //если не нашли - хешируем пароль
+      const userRole = await Role.findOne({value: 'USER'}); // нашли роль
+      const user = new User({username, password: hashPassowrd, roles: [userRole.value]}); // создали юзера
+      await user.save(); // сохраняем в базу
+      return res.json({message: 'User registration success'}); // вернули ответ на клиент
     } catch (e) {
-
+      console.log(e);
+      res.status(400).json({message: 'Registration error'})
     }
   }
 
@@ -15,7 +26,8 @@ class authController {
     try {
 
     } catch (e) {
-
+      console.log(e);
+      res.status(400).json({message: 'Login error'})
     }
   }
 
